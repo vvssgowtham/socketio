73be7f9cb6a5d83474ae73188a615e9e000b6530
vvssgowtham <gowthamvvss@gmail.com>
2024-01-19 18:50:48 +0530

    Complete Basic guide on how do socket.io is used.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import io from 'socket.io-client';
+import {useEffect, useState} from "react";
+
+//to establish the connection to socket io present in the backend
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  //room states
+  const [room,setRoom] = useState("");
+
+  //message states
+  const [message,setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const joinRoom = () => {
+    if(room !== ""){
+      socket.emit("join_room",room);
+    }
+  }
+
+  const sendMessage = () => {
+    socket.emit("send_message",{message : message,room});    
+  };
+
+  //the way we actually listen to that event obtained from backend is through using "useEffect" hook
+  useEffect(()=> {
+    socket.on("receive_message",(data) => {
+      setMessageReceived(data.message)
+    })
+    // eslint-disable-next-line
+  },[socket])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder="Room Number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}> Join Room</button>
+      <input
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <button onClick={sendMessage}> Send Message</button>
+      <h1> Message:</h1>
+      {messageReceived}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -12,14 +12,36 @@ app.use(cors());
 const server = http.createServer(app);
 
 //io is the variable which we will use to work on any related backend
-//Server is class and server is the http connected one 
-const io = new Server(server,{
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET","POST"]
-    }
+//Server is class and server is the http connected one
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-server.listen(3001, ()=> {
-    console.log("Listening on port 3001");
-})
+//to listen to an event we use on
+io.on("connection", (socket) => {
+  //console.log(`User Connected: ${socket.id}`);//this statement is just for checking whether server is running or not
+
+  //creating room so that specified devices can only communicate with eachother
+  //for this there is a specific event called "join_room"
+  socket.on("join_room", (data) => {
+    socket.join(data); //data is nothing but we are specifying room number something like that.
+  });
+  //on the backend means here we are listening to the event means we are listening to frontend i.e we are listening to frontend that is which room should this particular device should join.
+
+  socket.on("send_message", (data) => {
+    //broadcast will helps in delivering our message to every device which is connected to that port.
+    //in the first argument we specify event and in the second we use the necessary 1.
+    //socket.broadcast.emit("receive_message", data);
+    socket.to(data.room).emit("receive_message", data);//this means we are emitting the event to the specific room that we are part of.
+});
+});
+
+server.listen(3001, () => {
+  console.log("Listening on port 3001");
+});
+
+//The way socket.io works is u create certain events and u name those events and basically u can either listen to those events or emit an event.
+//Emit means i am sending or emitting some sort of data to all the people who were listening to that specific event including sending some data back.
